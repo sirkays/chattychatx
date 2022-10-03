9e74954f79fcbf36ff3725c1d82ac771e091f4ed
Kayode Johnson <attehkayode2@gmail.com>
2022-10-03 13:18:55 +0200

    simple api without REST api

--- chat/urls.py
+++ chat/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
+    path('set_room/', views.set_room, name='set_room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse,HttpResponse
 
 def index(request):
     return render(request, 'chat/index.html', {})
@@ -9,3 +9,7 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+def set_room(request):
+    room = request.POST.get("room")
+    return JsonResponse({"status":room})
