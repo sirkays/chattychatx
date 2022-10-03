3577345d9e77d070e32c272e9fecf902d2b3652d
Kayode Johnson <attehkayode2@gmail.com>
2022-10-03 14:46:20 +0200

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -13,4 +13,4 @@ def room(request, room_name):
 def set_room(request):
     room = request.POST.get("room")
     room2 = request.GET.get("room")
-    return JsonResponse({"status":room, "roomw":request.method})
+    return JsonResponse({"status":room, "roomw":request.method,"room2":room2})
