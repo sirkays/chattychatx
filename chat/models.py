aa5a70726e955fee3ca99aa39c72f30e1e7940b9
Kayode Johnson <attehkayode2@gmail.com>
2022-10-03 15:18:37 +0200

    creating rooms1

--- chat/models.py
+++ chat/models.py
@@ -5,3 +5,12 @@ class ChatRoom(models.Model):
     title = models.CharField(max_length=200)
     def __str__(self):
         return self.title
+
+    @classmethod
+    def get_room(cls,room,mode=True):
+        try:
+            return cls.objects.get(title=room)
+        except ObjectDoesNotExist:
+            if mode:
+                return cls.objects.create(title=room)
+            return False
--- chat/views.py
+++ chat/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from .models import ChatRoom
 def index(request):
     return render(request, 'chat/index.html', {})
 
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name
-    })
+    chat_room = ChatRoom.get_room(request.POST.get("room"),False)
+    if chat_room:
+        return render(request, 'chat/room.html', {
+            'room_name': chat_room
+        })
+    return JsonResponse({})
 
 @csrf_exempt
 def set_room(request):
-    if "https://kuboc.rextexh.com/" in request.META['HTTP_HOST']:
-        room = request.POST.get("room")
-    return JsonResponse({"status":room})
+    room= None
+    if "https://kuboc.rextexh.com/" in request.META['HTTP_HOST'] or "http://127.0.0.1:8000/" in request.META['HTTP_HOST']:
+        chat_room = ChatRoom.get_room(request.POST.get("room"))
+    return JsonResponse({"status":chat_room})
